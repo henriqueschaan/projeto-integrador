6160fc0f9b9979528a410bad09d2d648c82a5ed5
Henrique Schaan <102075535+henriqueschaan@users.noreply.github.com>
2023-11-03 13:59:34 -0300

    .

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { useState } from 'react';
 
 export function Form() {
     const [name, setName] = useState('');
-    const [age, setAge] = useState(0);
-    const [weight, setWeight] = useState(0);
-    const [height, setHeight] = useState(0);
-    const [exercise, setExercise] = useState(0);
-    const [stress, setStress] = useState(0);
+    const [age, setAge] = useState('');
+    const [weight, setWeight] = useState('');
+    const [height, setHeight] = useState('');
+    const [exercise, setExercise] = useState('');
+    const [stress, setStress] = useState('');
 
     function handleForm(event) {
         event.preventDefault();
@@ -28,16 +29,45 @@ export function Form() {
                     type="text"
                     placeholder="Digite seu nome"
                     value={name}
+                    onChange={(e) => setName(e.target.value)}
                     className="border border-gray-300 rounded-lg p-2 mb-2"
                 />
                 <label htmlFor="age">Idade</label>
-                <input type="text" />
+                <input
+                    id="age"
+                    type="text"
+                    placeholder="Digite sua idade"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                    className="border border-gray-300 rounded-lg p-2 mb-2"
+                />
                 <label htmlFor="weight">Peso</label>
-                <input type="text" />
+                <input
+                    id="weight"
+                    type="text"
+                    placeholder="Digite seu peso"
+                    value={weight}
+                    onChange={(e) => setWeight(e.target.value)}
+                    className="border border-gray-300 rounded-lg p-2 mb-2"
+                />
                 <label htmlFor="height">Altura</label>
-                <input type="text" />
+                <input
+                    id="height"
+                    type="text"
+                    placeholder="Digite sua altura"
+                    value={height}
+                    onChange={(e) => setHeight(e.target.value)}
+                    className="border border-gray-300 rounded-lg p-2 mb-2"
+                />
                 <label htmlFor="exercise">Nível de atividade física</label>
-                <input type="text" />
+                <input
+                    id="exercise"
+                    type="text"
+                    placeholder="Digite seu nível de atividade física"
+                    value={exercise}
+                    onChange={(e) => setExercise(e.target.value)}
+                    className="border border-gray-300 rounded-lg p-2 mb-2"
+                />
                 <label htmlFor="stress">Nível de estresse</label>
                 <input type="text" />
                 <button type="submit" className="flex bg-green-500 text-white items-center justify-center w-32 h-7 rounded-lg font-semibold">Avançar</button>
